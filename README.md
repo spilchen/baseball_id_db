01de875626831a01bb9d450388cc48b5e384ee81
Matt Spilchen <matt.spilchen@microfocus.com>
2022-03-13 13:21:42 -0300

    Add master.csv

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # baseball_id_db
-A .csv database that maps baseball players among different online sources
+A .csv database that maps baseball players among different online sources.
+
+There used to be a great database at https://crunchtimebaseball.com that published a mapping of a baseball player among different online sources.  You could look up a player by MLB ID, then be able to get the ID for the player at Yahoo!, CBS, ESPN, etc.  Sadly that database is no longer maintained.  This repo is an attempt to resurrect that map.  Contributions are welcome. 
---
+++ add_to_master.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+
+mlb_id=""
+print("name: ")
+mlb_name=input()
+print("pos: ")
+mlb_pos=input()
+print("bats: ")
+bats=input()
+print("throws: ")
+throws=input()
+print("birth_date: ")
+birth_date=input()
+bp_id=""
+bref_id=""
+bref_name=mlb_name
+cbs_id=""
+cbs_name=mlb_name
+cbs_pos=mlb_pos
+espn_id=""
+espn_name=mlb_name
+espn_pos=mlb_pos
+print("fg_id: ")
+fg_id=input()
+fg_name=mlb_name
+fg_pos=mlb_pos
+lahman_id=""
+nfbc_id=""
+nfbc_name=mlb_name
+nfbc_pos=mlb_pos
+retro_id=""
+retro_name=mlb_name
+print("yahoo_id: ")
+yahoo_id=input()
+yahoo_name=mlb_name
+ottoneu_id=""
+ottoneu_name=mlb_name
+ottoneu_pos=mlb_pos
+rotowire_id=""
+rotowire_name=mlb_name
+rotowire_pos=mlb_pos
+
+print(f"{mlb_id},{mlb_name},{mlb_pos},{bats},{throws},{birth_date},{bp_id},{bref_id},{bref_name},{cbs_id},{cbs_name},{cbs_pos},{espn_id},{espn_name},{espn_pos},{fg_id},{fg_name},{fg_pos},{lahman_id},{nfbc_id},{nfbc_name},{nfbc_pos},{retro_id},{retro_name},{yahoo_id},{yahoo_name},{ottoneu_id},{ottoneu_name},{ottoneu_pos},{rotowire_id},{rotowire_name},{rotowire_pos}")
